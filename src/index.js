e653ed9eb5c373303a391dfb9918eff5f23d1aa3
Guy Farley <guy.e.farley@gmail.com>
2022-08-27 10:28:02 -0700

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,9 @@
+'use strict';
+
+import HelloWorldButton from './components/hello-world-button/hello-world-button';
+import Heading from './components/heading/heading';
+
+const heading = new Heading();
+heading.render();
+const helloWorldButton = new HelloWorldButton();
+helloWorldButton.render();
