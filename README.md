0c5a9f91ebca223a460e00bd181dca757434510e
Guy Farley <guy.e.farley@gmail.com>
2022-08-28 10:34:24 -0700

    Extracting common dependencies for code splitting

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# webpack
+# webpack course
+
+Udemy course on Webpack, to be used for building a micro-frontend project
--- src/lassen.js
+++ src/lassen.js
@@ -1,7 +1,8 @@
 import Heading from './components/heading/heading';
 import LassenImage from './components/lassen-image/lassen-image';
+import _ from 'lodash';
 
 const heading = new Heading();
-heading.render();
+heading.render(_.upperFirst('lassen'));
 const lassenImage = new LassenImage();
 lassenImage.render();
